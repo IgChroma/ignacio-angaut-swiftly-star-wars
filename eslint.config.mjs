eef3f9fe6b7c06fe9c3b14b8f8c2eda30ca6c24d
IgChroma <54694652+IgChroma@users.noreply.github.com>
2024-07-26 20:40:22 -0300

    add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,6 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import config from "eslint-config-standard";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  ...[].concat(config),
 ];
