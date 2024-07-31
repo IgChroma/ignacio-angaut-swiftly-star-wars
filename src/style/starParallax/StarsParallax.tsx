6e15dc1a4596954b52021df49e2382dfea5e4b3c
IgChroma <54694652+IgChroma@users.noreply.github.com>
2024-07-31 19:39:27 -0300

    center stars

--- src/style/starParallax/StarsParallax.tsx
+++ src/style/starParallax/StarsParallax.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 import "./StarParallax.css";
 
 const StarParallax = () => {
-    return <>
+    return <div className='starrParallaxContainer'>
         <div id='stars'></div>
         <div id='stars2'></div>
         <div id='stars3'></div>
-    </>
+    </div>
 }
 
 export default StarParallax;
